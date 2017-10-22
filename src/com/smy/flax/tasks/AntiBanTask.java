ed2066e548d733b6bd398a302da8b294574596c0
Smy <benas512@gmail.com>
2017-10-22 12:42:12 -0700

    Updated a basic anti ban(pattern) class.

--- src/com/smy/flax/tasks/AntiBanTask.java
+++ src/com/smy/flax/tasks/AntiBanTask.java
@@ -1,9 +1,14 @@
 package com.smy.flax.tasks;
 
 import com.smy.flax.Task;
+import org.osbot.rs07.api.model.Player;
 import org.osbot.rs07.script.MethodProvider;
 
+import java.util.List;
+
 public class AntiBanTask extends Task {
+
+    private long lastOutOfScreen;
 
     public AntiBanTask(MethodProvider api) {
         super(api);
@@ -16,7 +21,31 @@ public class AntiBanTask extends Task {
 
     @Override
     public void process() throws InterruptedException {
-        int rnd = api.random(0, 100);
+        int rnd = MethodProvider.random(0,2);
+        int luck = MethodProvider.random(0, 100);
 
+        if(luck < 25){
+            switch(rnd){
+                case 0:     /*Move mouse out of screen*/
+                    api.mouse.moveOutsideScreen();
+                    break;
+                case 1:     /*Hovers and right clicks on random players*/
+                    List<Player> playerList = api.players.getAll();
+                    int right = MethodProvider.random(0,playerList.size());
+
+                    Player p = playerList.get(right);
+
+                    if(p != null){
+                        if(p.isVisible()){
+                            api.getMouse().click(p.getX(),p.getY(), true);
+                        } else {
+                            api.getCamera().toEntity(p);
+
+                            api.getMouse().click(p.getX(),p.getY(), true);
+                        }
+                    }
+                    break;
+            }
+        }
     }
 }
