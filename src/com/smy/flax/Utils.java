df1aa40701d76d3c67a500cfc89d2dcd52110233
Smy <benas512@gmail.com>
2017-10-25 09:26:53 -0700

    Added startTime variable for Painting.

--- src/com/smy/flax/Utils.java
+++ src/com/smy/flax/Utils.java
@@ -2,9 +2,10 @@ package com.smy.flax;
 
 public class Utils {
 	
-	public static long currentXp, latestXp, timeOnXpUpdate;
+	public static long currentXp, latestXp, timeOnXpUpdate, startTime = 0;
 	
 	public static boolean stopScript = false;
 	public static boolean mule = false;
 	public static boolean isSpinning = false;
+	public static boolean canStart = false;
 }
