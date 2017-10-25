4f43007a1e2b92fd48360a583e255491b77673a3
Smy <benas512@gmail.com>
2017-10-25 09:25:23 -0700

    Initial GUI class release.

---
+++ src/com/smy/flax/GUI.java
@@ -0,0 +1,67 @@
+package com.smy.flax;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class GUI extends JFrame{
+
+    private JPanel contentPane;
+
+    private JRadioButton radiobutt_logout, radiobutt_ge;
+    private JButton button_start;
+
+    public GUI() {
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent arg0) {
+                Utils.stopScript = true;
+            }
+        });
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setBounds(100, 100, 310, 180);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+        setVisible(true);
+
+        radiobutt_logout = new JRadioButton("Log out after finish");
+        radiobutt_logout.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(radiobutt_ge.isEnabled())
+                    radiobutt_ge.setEnabled(false);
+                else
+                    radiobutt_ge.setEnabled(true);
+            }
+        });
+        radiobutt_logout.setBounds(6, 7, 146, 23);
+        contentPane.add(radiobutt_logout);
+
+        radiobutt_ge = new JRadioButton("Go to GE after finish");
+        radiobutt_ge.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent arg0) {
+                if(radiobutt_logout.isEnabled())
+                    radiobutt_logout.setEnabled(false);
+                else
+                    radiobutt_logout.setEnabled(true);
+            }
+        });
+        radiobutt_ge.setBounds(6, 33, 146, 23);
+        contentPane.add(radiobutt_ge);
+
+        button_start = new JButton("Start");
+        button_start.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Utils.canStart = true;
+                Utils.startTime = System.currentTimeMillis();
+            }
+        });
+        button_start.setBounds(199, 7, 89, 23);
+        contentPane.add(button_start);
+    }
+
+}
