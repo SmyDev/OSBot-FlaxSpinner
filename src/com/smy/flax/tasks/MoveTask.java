d7ca31f0b08a278ebf7f54ba1a10a8e433c12f92
Smy <benas512@gmail.com>
2017-10-28 13:11:22 -0700

    Made banking area a bit smaller.

--- src/com/smy/flax/tasks/MoveTask.java
+++ src/com/smy/flax/tasks/MoveTask.java
@@ -9,7 +9,7 @@ import com.smy.flax.Utils;
 
 public class MoveTask extends Task{
 	
-	private Area BANK_AREA = new Area(new Position(3207, 3220, 2),
+	private Area BANK_AREA = new Area(new Position(3208, 3220, 2),
 			new Position(3209, 3218, 2));
 	
 	private Area SPIN_AREA = new Area(new Position(3209, 3213, 1),
